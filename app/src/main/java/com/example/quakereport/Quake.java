1609d343425930e60d8c8fae42f0136d3c5bacd4
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-04 17:18:30 +0300

    Quake Class

--- app/src/main/java/com/example/quakereport/Quake.java
+++ app/src/main/java/com/example/quakereport/Quake.java
@@ -1,4 +1,27 @@
 package com.example.quakereport;
 
 public class Quake {
+
+    private String mMagnitude, mLocation, mDate;
+
+    public Quake() {
+    }
+
+    public Quake(String mMagnitude, String mLocation, String mDate) {
+        this.mMagnitude = mMagnitude;
+        this.mLocation = mLocation;
+        this.mDate = mDate;
+    }
+
+    public String getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
 }
--- app/src/main/java/com/example/quakereport/QuakeAdapter.java
+++ app/src/main/java/com/example/quakereport/QuakeAdapter.java
@@ -2,26 +2,5 @@ package com.example.quakereport;
 
 public class QuakeAdapter {
 
-    private String mMagnitude, mLocation, mDate;
 
-    public QuakeAdapter() {
-    }
-
-    public QuakeAdapter(String mMagnitude, String mLocation, String mDate) {
-        this.mMagnitude = mMagnitude;
-        this.mLocation = mLocation;
-        this.mDate = mDate;
-    }
-
-    public String getmMagnitude() {
-        return mMagnitude;
-    }
-
-    public String getmLocation() {
-        return mLocation;
-    }
-
-    public String getmDate() {
-        return mDate;
-    }
 }
