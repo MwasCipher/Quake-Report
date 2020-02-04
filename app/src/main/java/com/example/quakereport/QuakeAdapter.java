1c3941b65741253a6ff0c53781d31ecc9e103b27
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-04 17:24:02 +0300

    Quake Item

--- app/src/main/java/com/example/quakereport/QuakeAdapter.java
+++ app/src/main/java/com/example/quakereport/QuakeAdapter.java
@@ -1,6 +1,28 @@
 package com.example.quakereport;
 
-public class QuakeAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class QuakeAdapter extends ArrayAdapter<Quake> {
 
 
+    public QuakeAdapter(@NonNull Context context, int resource, @NonNull List<Quake> quakes) {
+        super(context, resource, quakes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+
+        return super.getView(position, convertView, parent);
+    }
 }
