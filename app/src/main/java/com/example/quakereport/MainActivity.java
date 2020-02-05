45727a63716bcc30ca3b9287783a80d6a7eea0e7
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-05 18:13:30 +0300

    Query Utils

--- app/src/main/java/com/example/quakereport/MainActivity.java
+++ app/src/main/java/com/example/quakereport/MainActivity.java
@@ -18,24 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       ArrayList<Quake> quakes = new ArrayList<>();
-       quakes.add(new Quake("71", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("7", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("1", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("17", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("6", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("6", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("9", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("5", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("9", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("3", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("8", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("1", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("7", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("6", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("3", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("8", "Riruta", "9 Sep 2091"));
-       quakes.add(new Quake("2", "Riruta", "9 Sep 2091"));
+       ArrayList<Quake> quakes = QueryUtils.extractEarthQuakes();
 
        listView = findViewById(R.id.list_view);
        arrayAdapter = new QuakeAdapter(getApplicationContext(), quakes);
